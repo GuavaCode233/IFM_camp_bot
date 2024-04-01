87637919a4ddf0d1910475e8c4162ece67810994
Guava <eugine940218@gmail.com>
2024-04-01 23:27:51 +0800

    ChangeDepositView development

--- Cogs/discord_ui.py
+++ Cogs/discord_ui.py
@@ -1,42 +1,72 @@
 from nextcord.ext import commands
 import nextcord as ntd
 
+from datetime import datetime
+
 from .utilities import AccessFile
 from .assets_manager import AssetsManager
 
 
-# class TeamDepositView(View):
-#     """小隊收支按鈕
-#     """
-
-#     @ntd.ui.button(label="加錢", style=ntd.ButtonStyle.green, emoji="➕")
-#     async def deposit_button_callback(self, button: Button, interaction: Interaction):
-#         await interaction.response.send_message("加錢啦", ephemeral=True, delete_after=3)
-
-#     @ntd.ui.button(label="扣錢", style=ntd.ButtonStyle.red, emoji="➖")
-#     async def withdraw_button_callback(self, button: Button, interaction: Interaction):
-#         await interaction.response.send_message("扣錢啦", ephemeral=True, delete_after=3)
-
-#     @ntd.ui.button(label="更改餘額", style=ntd.ButtonStyle.gray, emoji="🔑")
-#     async def change_button_callback(self, button: Button, interaction: Interaction):
-#         await interaction.response.send_message("改餘額", ephemeral=True, delete_after=3)
-
-
-class TestForm(ntd.ui.Modal):
-
-    def __init__(self):
-        super().__init__(title="Title")
-
-
 class ChangeDepositView(ntd.ui.View):
+    """變更小隊存款更能View。
     """
-    """
-    def __init__(self):
+
+    __slots__ = (
+        "embed_title",
+        "embed_description",
+        "mode_field_value",
+        "amount",
+        "author_name",
+        "author_icon",
+        "selected_team",
+        "selected_mode"
+    )
+
+    def __init__(
+            self,
+            author_name: str,
+            author_icon: ntd.Asset
+    ):
         super().__init__()
-        self.selected_team = None
+        # embed message
+        self.embed_title: str = "變更小隊存款"  # 變更第n小隊存款
+        self.embed_description: str | None = "請選擇小隊" # None
+        self.mode_field_value: str = "請選擇變更模式"   # None
+        self.amount: str | int = "請輸入金額"   # 金額: int
+        self.author_name = author_name
+        self.author_icon = author_icon
+        # slect status
+        self.selected_team: str | None = None
+        self.selected_mode: str | None = None
+
+    def status_embed(self) -> ntd.Embed:
+        """用於編排嵌入訊息
+        """
+
+        embed = ntd.Embed(
+            colour=0x433274,
+            title=self.embed_title,
+            type="rich",
+            description=self.embed_description,
+            timestamp=datetime.now()
+        )
+        embed.add_field(
+            name="變更模式",
+            value=self.mode_field_value
+        )
+        embed.add_field(
+            name="變更金額",
+            value=self.amount
+        )
+        embed.set_footer(
+            text=self.author_name,
+            icon_url=self.author_icon
+        )
+
+        return embed
 
     @ntd.ui.select(
-        placeholder="Placehoder",
+        placeholder="選擇小隊",
         min_values=1,
         max_values=1,
         options=[
@@ -52,49 +82,108 @@ class ChangeDepositView(ntd.ui.View):
         select: ntd.ui.StringSelect, 
         interaction: ntd.Interaction
     ):
-        await interaction.response.send_message(
-            f"You choosed team: {select.values[0]}"
+        """小隊選取選單callback
+        """
+
+        self.embed_title = f"變更第{select.values[0]}小隊存款"
+        self.embed_description = None
+
+        await interaction.response.edit_message(
+            embed=self.status_embed()
         )
-        self.selected_team = select.values[0]
 
     
     @ntd.ui.select(
-    placeholder="Placehoder",
+    placeholder="選擇變更模式",
     min_values=1,
     max_values=1,
     options=[
         ntd.SelectOption(
             label="增加存款",
-            value="increase",
             description="輸入增加的金額。",
             emoji="➕"
         ),
         ntd.SelectOption(
             label="減少存款",
-            value="decrease",
             description="輸入減少的金額。",
             emoji="➖"
         ),
         ntd.SelectOption(
             label="更改存款餘額",
-            value="change",
             description="輸入改變的餘額。",
             emoji="🔑"
         )
         ]
-        )
-    async def type_select_callback(
+    )
+    async def mode_select_callback(
         self,
         select: ntd.ui.StringSelect, 
         interaction: ntd.Interaction
     ):
-        await interaction.response.send_message(
-            f"You choosed to {select.values[0]}" \
-            f"the deposit of team: {self.selected_team}."
+        """模式選取選單callback。
+        """
+
+        self.mode_field_value = select.values[0]
+        await interaction.response.edit_message(
+            view=self,
+            embed=self.status_embed()
         )
 
+    @ntd.ui.button(
+        label="輸入金額",
+        style=ntd.ButtonStyle.grey,
+        emoji="🪙"
+    )
+    async def input_amount_button_callback(
+        self,
+        button: ntd.ui.Button,
+        interaction: ntd.Interaction
+    ):
+        """輸入金額按鈕callback。
+        """
+        await interaction.response.send_modal(InputAmount(self))
 
 
+class InputAmount(ntd.ui.Modal):
+    """按下「輸入存款」按鈕後彈出的視窗。
+    """
+
+    def __init__(
+            self,
+            original_view: ChangeDepositView,
+            default_value: str | None = None
+    ):
+        super().__init__(title="請輸入金額")
+
+        self.original_view = original_view
+
+        self.amount = ntd.ui.TextInput(
+            label="請輸入金額",
+            style=ntd.TextInputStyle.short,
+            min_length=1,
+            max_length=6,
+            required=True,
+            default_value=default_value,
+            placeholder="輸入金額"
+        )
+        self.add_item(self.amount)
+
+    async def callback(self, interaction: ntd.Interaction):
+        try:
+            self.original_view.amount = int(self.amount.value)
+            await interaction.response.edit_message(
+                view=self.original_view,
+                embed=self.original_view.status_embed()
+            )
+        except ValueError:  # 防呆(輸入文字)
+            await interaction.response.send_message(
+                content="**金額請輸入數字!!!**",
+                delete_after=5,
+                ephemeral=True
+            )
+
+        self.stop()
+        
 class DiscordUI(commands.Cog, AccessFile):
     """控制Discord端的UI介面
     """
@@ -112,15 +201,21 @@ class DiscordUI(commands.Cog, AccessFile):
     async def test_ui(self, ctx: commands.Context):
         pass
 
-
     @ntd.slash_command(
             name="test_ui",
             description="For testing UIs",
             guild_ids=[1218130958536937492]
     )
     async def test_ui(self, interaction: ntd.Interaction):
+        view = ChangeDepositView(
+            interaction.user.display_name,
+            interaction.user.display_avatar
+        )
         await interaction.response.send_message(
-            view=ChangeDepositView()
+            view=view,
+            embed=view.status_embed(),
+            delete_after=180,
+            ephemeral=True
         )
 
     async def resend_assets_ui(self):
